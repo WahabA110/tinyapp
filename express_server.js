b84b9bf92649828519a5b5c550f3c16e70f7547b
Wahab Alamin <wahabalamin@gmail.com>
2021-09-03 16:32:12 -0400

    added a global users object to store and access the users in the app

--- express_server.js
+++ express_server.js
@@ -19,6 +19,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -37,6 +50,7 @@ app.get("/register", (req, res) => {
   }
   res.render("urls_register", templateVars)
 });
+
 
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
