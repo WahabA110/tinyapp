3279363c30c4bfe30a5c3e883d370782087185fe
Wahab Alamin <wahabalamin@gmail.com>
2021-09-05 18:46:04 -0400

    minor update. Just eslint to check for correct indentation and spacing

--- express_server.js
+++ express_server.js
@@ -18,30 +18,30 @@ app.use(cookieSession({
   keys: ['longKey']
 }));
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.random().toString(36).substr(2, 6);
-}
+};
 
-const urlsForUser = function (id) {
+const urlsForUser = function(id) {
   let obj = {};
   for (let key in urlDatabase) {
-    let url = urlDatabase[key]
+    let url = urlDatabase[key];
     if (url.userID === id) {
-      obj[key] = url
+      obj[key] = url;
     }
   }
   return obj;
-}
+};
 
-const ownerCheck = function (req, res) {
+const ownerCheck = function(req, res) {
   const currentUser = req.session.user_id;
   if (!currentUser) {
     res.send("Please <a href= '/login'>log in</a> or <a href= '/register'>register</a> first");
   }
   if (urlDatabase[req.params.shortURL].userID !== currentUser) {
-    res.send("URL does not match your account. Head back to <a href= '/urls'>urls</a>")
+    res.send("URL does not match your account. Head back to <a href= '/urls'>urls</a>");
   }
-}
+};
 
 const urlDatabase = {
   "b6UTxQ": {
